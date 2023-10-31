714d7dbf436992e4f9df542507c98c1332171cd9
[Hailey G] <[haileydgrace@gmail.com]>
2023-10-31 17:05:11 -0400

    basic html dropdown to begin values

--- algorithms/Decision-tree-graphic/decision-tree.js
+++ algorithms/Decision-tree-graphic/decision-tree.js
@@ -1,3 +1,12 @@
+
 function transportationOptions() {
     // Write code here to make the decision tree into an algorithm!
+    let firstResponse = document.querySelector('.weather-type')
+
+    firstResponse.addEventListener("change", function() {
+      let selectedWeather = firstResponse.value
+      console.log(selectedWeather)
+    })
   }
+
+  transportationOptions()
