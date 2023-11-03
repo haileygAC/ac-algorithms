0fa31ee7b90be3416f2b45b8c76080dd9a081da2
haileygAC <hailey@anniecannons.com>
2023-11-03 17:00:45 -0400

    Completed validate user

--- algorithms/3-Arrays-and-Objects/validateUser.js
+++ algorithms/3-Arrays-and-Objects/validateUser.js
@@ -18,7 +18,7 @@ Steps
  */
 
 function validateUserCredentials(users, inputUsername, inputPassword) {
-    
+    return users.some(user => user.username == inputUsername && user.password == inputPassword)
 }
 
 // Example usage
